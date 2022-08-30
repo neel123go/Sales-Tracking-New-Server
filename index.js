93116082935b446917325e74eda80e99c9885cc4
neel123go <ayounpaul1029@gmail.com>
2022-08-30 14:49:59 +0600

    use JWT Token for secure server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8elvquz.mongodb.net/?retryWrites=true&w=majority`;
+// function for verify JWT Token
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    } else {
+        const token = authHeader.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                return res.status(403).send({ message: 'Forbidden Access' });
+            } else {
+                req.decoded = decoded;
+                next();
+            }
+        })
+    }
+}
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8elvquz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
@@ -18,6 +36,15 @@ async function run() {
         await client.connect();
         const itemsCollection = client.db('Sales_Tracking_new').collection('inventory_items');
         const queriesCollection = client.db('Sales_Tracking_new').collection('queries');
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '1d'
+            });
+            res.send({ accessToken });
+        });
 
         // get all items
         app.get('/items', async (req, res) => {
@@ -66,11 +93,14 @@ async function run() {
         })
 
         // get items by user email
-        app.get('/myItems', async (req, res) => {
+        app.get('/myItems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const cursor = itemsCollection.find({ email: email });
-            const items = await cursor.toArray();
-            res.send(items);
+            if (decodedEmail === email) {
+                const cursor = itemsCollection.find({ email: email });
+                const items = await cursor.toArray();
+                res.send(items);
+            }
         });
 
         // add queries by user
